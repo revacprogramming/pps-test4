83f4832633d6d603e8510f35347830244ecf3f50
srinirad <srinirad@users.noreply.github.com>
2021-10-26 16:52:22 +0530

    Update p7original.c

---
+++ p7original.c
@@ -0,0 +1,6 @@
+/*
+Strings Write a program to find if the name of the camel is a nice name. 
+Camels name is nice if it has at least 2 vowels and 2 consonants in it. with four functions - 5 marks.
+*/
+
+
